0f13e43916f20da07b6dddcde7d4deb5cc972780
zxeni1 <zacxfitness@gmail.com>
2024-12-03 19:44:15 +1000

    feat. fetched movies from the API title

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/watchlist' element={<div>Watchlist</div>} />
+        <Route path='/details/:id' element={<div>details</div>} />
       </Routes>
     </Router>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,50 @@
+import axios from "axios";
 import React, { useState } from "react";
 
-export default function Home(){
+export default function Home() {
     const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
-    const fetchMovies = async() => {
-        // Using Fetch()
-        // const response = await fetch (`https://www.omdbapi.com/${apikey}/${query}`);
+    const fetchMovies = async () => {
+        // Using fetch()
+        // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`); 
 
-        // Using Axios()
-    }
+        // Using axios()
+        if (query) {
+            const response = await axios.get(`http://www.omdbapi.com/`, {
+                params: {
+                    apikey: 'd58efa1d',
+                    s: query
+                }
+            });
+            console.log(response.data);
+            setMovies(response.data.Search || []);
+        }
+       
+    };
 
     return (
         <div>
             <h1>Movie Search</h1>
             <input
-            type="text"
-            placeholder="Search for Movies..." 
+                type="text"
+                placeholder="Search for Movies..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
             />
-            <button>Search</button>
+            <button onClick={fetchMovies}>Search</button>
+
+            <div>
+                {
+                    movies.map((movie) => (
+                        <div key={movie.imdbID}>
+                            <h3>{movie.Title}</h3>
+                            <p>{movie.Year}</p>
+                        </div>  
+                    ))
+                }
+
+            </div>
         </div>
     )
 }
