9bca90356f1684d3c9859498dd4dcfb9191ec610
zxeni1 <zacxfitness@gmail.com>
2024-12-07 11:10:04 +1000

    CSS styling added

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,31 +1,31 @@
 import React from "react";
+
 import { useWatchlist } from "../context/WatchlistContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Watchlist = () => {
     // Fetch two out of three returns from the custom hook
     const { watchlist, removeFromWatchlist } = useWatchlist();
     const navigate = useNavigate();
-
     return (
-        <div>
+        <div style={{maxWidth: '18rem'}}>
             <button className="btn btn-secondary mb-3" onClick={() => navigate(-1)}>Go back</button>
             <h2>Your Watchlist</h2>
-            {console.log(watchlist)}
             {watchlist.length === 0 ? (
                 <p>Your watchlist is empty. Start adding some movies!</p>
             ) : (
                 <ul>
                     {watchlist.map((movie) => (
                         <li key={movie.imdbID}>
-                            <strong>{movie.Title}</strong> ({movie.Year})
-                            <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+                            <p><strong>{movie.Title}</strong> ({movie.Year})</p>
+                            <img src={movie.Poster} alt={`${movie.Title} Poster` } className='card-img-top' />
                             <button
                                 onClick={() => removeFromWatchlist(movie.imdbID)}
                                 style={{ color: 'red' }}
                             >
                                 Remove
                             </button>
+                            <Link to={`/details/${movie.imdbID}`} className="btn btn-primary">View Details</Link>
                         </li>
                     ))}
                 </ul>
